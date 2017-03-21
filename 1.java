bf1c4068248360f1b7a9847cc67af10a1c3dee70
Guoxuelong <Guoxuelong@users.noreply.github.com>
2017-03-21 13:48:33 +0800

    Add files via upload

---
+++ 1.java
@@ -0,0 +1,136 @@
+{
+    "reason": "查询成功",
+    "result": {
+        "title": "NBA2014-2015赛季_常规赛赛程赛果",
+        "statuslist": {
+            "st0": "未开赛",
+            "st1": "直播中",
+            "st2": "已结束"
+        },
+        "list": [
+            {
+                "title": "12-07 周日",
+                "tr": [
+                    {
+                        "link1text": "视频集锦",
+                        "link1url": "http://video.sina.com.cn/p/sports/k/v/2014-12-07/140064340433.html",
+                        "link2text": "技术统计",
+                        "link2url": "http://nba.sports.sina.com.cn/look_scores.php?id=2014120608",
+                        "player1": "76人",
+                        "player1logo": "http://www.sinaimg.cn/ty/deco/2013/1108/nbalogo/20.png",
+                        "player1logobig": "http://p4.qhimg.com/t01d3db357326a5f9b7.png",
+                        "player1url": "http://nba.sports.sina.com.cn/team.php?id=20",
+                        "player2": "活塞",
+                        "player2logo": "http://www.sinaimg.cn/ty/deco/2013/1108/nbalogo/8.png",
+                        "player2logobig": "http://p4.qhimg.com/t014bc2a39f1ff5a13a.png",
+                        "player2url": "http://nba.sports.sina.com.cn/team.php?id=8",
+                        "score": "108-101",
+                        "status": 2,
+                        "time": "12/07 08:30"
+                    },
+                    {
+                        "link1text": "视频集锦",
+                        "link1url": "http://video.sina.com.cn/p/sports/k/v/2014-12-07/122964340177.html",
+                        "link2text": "技术统计",
+                        "link2url": "http://nba.sports.sina.com.cn/look_scores.php?id=2014120604",
+                        "player1": "勇士",
+                        "player1logo": "http://www.sinaimg.cn/ty/deco/2013/1108/nbalogo/9.png",
+                        "player1logobig": "http://p7.qhimg.com/t01d2065c2ad5a47937.png",
+                        "player1url": "http://nba.sports.sina.com.cn/team.php?id=9",
+                        "player2": "公牛",
+                        "player2logo": "http://www.sinaimg.cn/ty/deco/2013/1108/nbalogo/4.png",
+                        "player2logobig": "http://p6.qhimg.com/t01be4b73bdda7dbb0b.png",
+                        "player2url": "http://nba.sports.sina.com.cn/team.php?id=4",
+                        "score": "112-102",
+                        "status": 2,
+                        "time": "12/07 09:00"
+                    },
+                    {
+                        "link1text": "视频集锦",
+                        "link1url": "http://video.sina.com.cn/z/sports/k/nba/141207phxhou/",
+                        "link2text": "技术统计",
+                        "link2url": "http://nba.sports.sina.com.cn/look_scores.php?id=2014120610",
+                        "player1": "太阳",
+                        "player1logo": "http://www.sinaimg.cn/ty/deco/2013/1108/nbalogo/21.png",
+                        "player1logobig": "http://p5.qhimg.com/t010dda40063e994c77.png",
+                        "player1url": "http://nba.sports.sina.com.cn/team.php?id=21",
+                        "player2": "火箭",
+                        "player2logo": "http://www.sinaimg.cn/ty/deco/2013/1108/nbalogo/10.png",
+                        "player2logobig": "http://p5.qhimg.com/t015ca41a7ba9c358b6.png",
+                        "player2url": "http://nba.sports.sina.com.cn/team.php?id=10",
+                        "score": "95-100",
+                        "status": 2,
+                        "time": "12/07 09:00"
+                    },
+                    {
+                        "link1text": "视频集锦",
+                        "link1url": "http://video.sina.com.cn/p/sports/k/v/2014-12-07/134864340393.html",
+                        "link2text": "技术统计",
+                        "link2url": "http://nba.sports.sina.com.cn/look_scores.php?id=2014120624",
+                        "player1": "森林狼",
+                        "player1logo": "http://www.sinaimg.cn/ty/deco/2013/1108/nbalogo/16.png",
+                        "player1logobig": "http://p2.qhimg.com/t01a05379822b554e07.png",
+                        "player1url": "http://nba.sports.sina.com.cn/team.php?id=16",
+                        "player2": "马刺",
+                        "player2logo": "http://www.sinaimg.cn/ty/deco/2013/1108/nbalogo/24.png",
+                        "player2logobig": "http://p8.qhimg.com/t019450aafe5d71656d.png",
+                        "player2url": "http://nba.sports.sina.com.cn/team.php?id=24",
+                        "score": "101-123",
+                        "status": 2,
+                        "time": "12/07 09:30"
+                    },
+                    {
+                        "link1text": "视频集锦",
+                        "link1url": "http://video.sina.com.cn/p/sports/k/v/2014-12-07/144764340623.html",
+                        "link2text": "技术统计",
+                        "link2url": "http://nba.sports.sina.com.cn/look_scores.php?id=2014120623",
+                        "player1": "魔术",
+                        "player1logo": "http://www.sinaimg.cn/ty/deco/2013/1108/nbalogo/19.png",
+                        "player1logobig": "http://p5.qhimg.com/t0167317f2b1249e741.png",
+                        "player1url": "http://nba.sports.sina.com.cn/team.php?id=19",
+                        "player2": "国王",
+                        "player2logo": "http://www.sinaimg.cn/ty/deco/2013/1108/nbalogo/23.png",
+                        "player2logobig": "http://p7.qhimg.com/t018d78076cc925f692.png",
+                        "player2url": "http://nba.sports.sina.com.cn/team.php?id=23",
+                        "score": "105-96",
+                        "status": 2,
+                        "time": "12/07 11:00"
+                    },
+                    {
+                        "link1text": "视频集锦",
+                        "link1url": "http://video.sina.com.cn/z/sports/k/nba/141207noplac/",
+                        "link2text": "技术统计",
+                        "link2url": "http://nba.sports.sina.com.cn/look_scores.php?id=2014120612",
+                        "player1": "鹈鹕",
+                        "player1logo": "http://www.sinaimg.cn/ty/deco/2013/1108/nbalogo/3.png",
+                        "player1logobig": "http://p5.qhimg.com/t01d4114e4e03806e71.png",
+                        "player1url": "http://nba.sports.sina.com.cn/team.php?id=3",
+                        "player2": "快船",
+                        "player2logo": "http://www.sinaimg.cn/ty/deco/2013/1108/nbalogo/12.png",
+                        "player2logobig": "http://p6.qhimg.com/t0198ca8635a51d759d.png",
+                        "player2url": "http://nba.sports.sina.com.cn/team.php?id=12",
+                        "score": "100-120",
+                        "status": 2,
+                        "time": "12/07 11:30"
+                    }
+                ],
+                "bottomlink": [
+                    {
+                        "text": "常规赛赛程",
+                        "url": "http://nba.sports.sina.com.cn/match_result.php"
+                    },
+                    {
+                        "text": "电视转播表",
+                        "url": "http://nba.sports.sina.com.cn/showtv.php?"
+                    },
+                    {
+                        "text": "球队排名",
+                        "url": "http://nba.sports.sina.com.cn/league_order.php?"
+                    },
+                    {
+                        "text": "球员排名",
+                        "url": "http://nba.sports.sina.com.cn/playerstats.php?"
+                    }
+                ]
+},
+}
