22662ca66bd7703eeb3a2fd210ac0f4979d686c0
Guoxuelong <Guoxuelong@users.noreply.github.com>
2017-03-22 17:14:39 +0800

    Update lunboPic2.java

--- lunboPic2.java
+++ lunboPic2.java
@@ -1,21 +1,3 @@
 ﻿{
-	"code":"100",
-	"message": "成功",
-		"list":[
-			{
-				"pic":"http://p1.bpimg.com/567571/a488a669d247c3a6.jpg"
-			},
-			{
-				"pic":"http://p1.bqimg.com/567571/7678d510de75ce90.jpg"
-			},
-			{
-				"pic":"http://p1.bpimg.com/567571/93cfe5ceff772663.jpg"
-			},
-			{
-				"pic":"http://p1.bqimg.com/567571/3f36d387de29c10a.jpg"
-			},
-			{
-				"pic":"http://p1.bqimg.com/567571/21902a096fb7ce7a.jpg"
-			},
-		]
+	"code":"100"
 }
