f1064017867d40fd0e8b40adbdf2fe797fa1ba73
Guoxuelong <Guoxuelong@users.noreply.github.com>
2017-03-22 17:00:55 +0800

    Add files via upload

---
+++ pic.java
@@ -0,0 +1,23 @@
+﻿{
+    "reason": "查询成功",
+    "result": {
+        "teammatch": [
+            {
+                "name": "老鹰",
+                "pic":"http://p1.bpimg.com/567571/a488a669d247c3a6.jpg"
+            },
+            {
+                "name": "凯尔特人",
+                "pic":"http://p1.bqimg.com/567571/7678d510de75ce90.jpg"
+            },
+            {
+                "name": "爵士",
+                "pic":"http://p1.bpimg.com/567571/93cfe5ceff772663.jpg"
+            },
+            {
+                "name": "奇才",
+                "pic":"http://p1.bqimg.com/567571/21902a096fb7ce7a.jpg"
+            }
+        ]
+    }
+}
